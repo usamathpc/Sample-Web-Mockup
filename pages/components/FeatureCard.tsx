d82baf27f5d21874e32c775bf076c554196878e6
Usamath <usamathpallikkachalil@gmail.com>
2022-03-11 20:17:05 +0530

    feat: component outline

---
+++ pages/components/FeatureCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeatureCard = () => {
+  return <div>FeatureCard</div>;
+};
+
+export default FeatureCard;
---
+++ pages/components/RecentWorkCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecentWorkCard = () => {
+  return <div>RecentWorkCard</div>;
+};
+
+export default RecentWorkCard;
