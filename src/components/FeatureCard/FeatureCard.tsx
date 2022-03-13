ccade7be6bc593b25d8cbfdcf90b513f2d3cbe59
Usamath <usamathpallikkachalil@gmail.com>
2022-03-14 03:40:09 +0530

    features and feature card

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -1,7 +1,32 @@
+import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
-const FeatureCard = () => {
-  return <div>FeatureCard</div>;
+type Props = {
+  title: string;
+  icon: any;
+  description: string;
 };
 
-export default FeatureCard;
+export const FeatureCard = (props: Props) => {
+  return (
+    <Box
+      style={{
+        padding: 10,
+        borderRadius: 10,
+        boxShadow: `rgba(149, 157, 165, 0.2) 0px 8px 24px`,
+      }}
+    >
+      <Flex alignItems={"center"} justifyContent="space-between">
+        <Text variant="h3">{props.title}</Text>
+        {props.icon}
+      </Flex>
+      <Divider
+        sx={{
+          mt: 2,
+          mb: 2,
+        }}
+      />
+      <Text>{props.description}</Text>
+    </Box>
+  );
+};
--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,7 +1,37 @@
+import { SimpleGrid } from "@chakra-ui/react";
 import React from "react";
+import { FeatureCard } from "../FeatureCard/FeatureCard";
+
+import { SiGoogleanalytics } from "react-icons/si";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { GoSettings } from "react-icons/go";
+import styles from "./features.module.scss";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <SimpleGrid className={styles.features} columns={[1, 2, 4]} spacing="40px">
+      <FeatureCard
+        icon={<SiGoogleanalytics />}
+        title="Fast Analytics"
+        description="Connect and visualise your data in minutes. The MissionOS system is significantly faster than existing solutions. Monitor your data feeds in real-time."
+      />
+      <FeatureCard
+        icon={<MdOutlineSpaceDashboard />}
+        title="Smart Dashboards"
+        description="Combine multiple views of data to gain a richer insight. Best practices of data visualisation are built-in."
+      />
+      <FeatureCard
+        icon={<GoSettings />}
+        title="Configurable"
+        description="Utilise the 100% configurable MissionOS user-interface, gain access to a library of templates & customisation tools."
+      />
+      <FeatureCard
+        icon={<GoSettings />}
+        title="Configurable"
+        description="Utilise the 100% configurable MissionOS user-interface, gain access to a library of templates & customisation tools."
+      />
+    </SimpleGrid>
+  );
 };
 
 export default Features;
