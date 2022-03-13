ddb0a6075c07d0108a5d78a95224366df87fa791
Usamath <usamathpallikkachalil@gmail.com>
2022-03-14 03:39:57 +0530

    update index, home

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Features from "../src/components/Features/Features";
 import { Header } from "../src/components/Header/Header";
+import Hero from "../src/components/Hero/Hero";
 import Navbar from "../src/components/Navbar/Navbar";
 
 const home = () => {
   return (
     <>
       <Header />
+      <Hero />
+      <Features />
     </>
   );
 };
