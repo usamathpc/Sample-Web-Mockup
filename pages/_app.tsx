0017f43b986206c8d535859922052c1aeebc299b
Usamath <usamathpallikkachalil@gmail.com>
2022-03-15 22:43:33 +0530

    add brand colors to chakra theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,11 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
-
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
+import { theme } from "../src/theme";
+import "../src/theme/styles.css";
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
       <Component {...pageProps} />
     </ChakraProvider>
   );
