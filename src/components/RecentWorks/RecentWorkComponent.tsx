0fa190f83e5fff4f9bf7cf7a0d1d2f16cad734f1
Usamath <usamathpallikkachalil@gmail.com>
2022-03-16 00:13:53 +0530

    fix recent works styles

--- src/components/RecentWorks/RecentWorkComponent.tsx
+++ src/components/RecentWorks/RecentWorkComponent.tsx
@@ -1,12 +1,22 @@
+import { Box, SimpleGrid, Tag, Text } from "@chakra-ui/react";
 import React from "react";
-import { Box, Divider, Flex, SimpleGrid, Tag, Text } from "@chakra-ui/react";
-import { MdAttachMoney } from "react-icons/md";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 import styles from "./recentworks.module.scss";
 import { StatsCard } from "./StatsCard";
 
-import { useMediaQuery } from "../../hooks/useMediaQuery";
+interface StatsCard {
+  title: string;
+  icon: any;
+  value: string;
+}
 
-type Props = {};
+type Props = {
+  imageUrl: string;
+  title: string;
+  description: string;
+  tags: string[];
+  stats: StatsCard[];
+};
 
 export const RecentWorkComponent = (props: Props) => {
   const isGreaterThan900px = useMediaQuery("900");
@@ -17,61 +27,35 @@ export const RecentWorkComponent = (props: Props) => {
       }}
     >
       <Box className={styles.recentWorksItemImageContainer}>
-        <img src="/sydneyMetro.jpg" className={styles.recentWorksItemImage} />
-        <Text className={styles.recentWorksItemTitle}>Sydney Metro</Text>
+        <img src={props.imageUrl} className={styles.recentWorksItemImage} />
+        <Text className={styles.recentWorksItemTitle}>{props.title}</Text>
       </Box>
       <Box className={styles.recentWorksItemDataContainer}>
         <Text className={styles.recentWorksItemTitleForSmallScreen}>
-          Sydney Metro
+          {props.title}
         </Text>
         <Box display={"flex"} justifyContent="flex-end">
-          <Tag size={isGreaterThan900px ? "lg" : "sm"} ml={2}>
-            Excavation
-          </Tag>
-          <Tag ml={2}>Sydney</Tag>
-          <Tag ml={2}>Australia</Tag>
-          <Tag ml={2}>Metro Level 2</Tag>
+          {props.tags.map((tag, index) => (
+            <Tag key={index} size={isGreaterThan900px ? "lg" : "sm"} ml={2}>
+              {tag}
+            </Tag>
+          ))}
         </Box>
 
         <Box className={styles.recentWorksItemDescriptionContainer}>
           <Text className={styles.recentWorksItemDescription}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-            quidem ab molestiae impedit eveniet explicabo. Doloribus sunt
-            placeat minus natus commodi laboriosam delectus. Exercitationem eius
-            iusto facere tempora optio pariatur?
+            {props.description}
           </Text>
           <Box className={styles.recentWorksItemTags}>
             <SimpleGrid columns={[1, 1, 3, 3, 3]} spacing={5}>
-              <StatsCard
-                icon={<MdAttachMoney />}
-                title="Budget"
-                value="1 Billion"
-              />
-              <StatsCard
-                icon={<MdAttachMoney />}
-                title="Instruments Monitoring"
-                value="7500+"
-              />
-              <StatsCard
-                icon={<MdAttachMoney />}
-                title="Instruments Records"
-                value="44M+"
-              />
-              <StatsCard
-                icon={<MdAttachMoney />}
-                title="Monitoring Reports"
-                value="40+ Daily"
-              />
-              <StatsCard
-                icon={<MdAttachMoney />}
-                title="Instruments Records"
-                value="44M+"
-              />
-              <StatsCard
-                icon={<MdAttachMoney />}
-                title="Monitoring Reports"
-                value="40+ Daily"
-              />
+              {props.stats.map((stat, index) => (
+                <StatsCard
+                  key={index}
+                  title={stat.title}
+                  icon={stat.icon}
+                  value={stat.value}
+                />
+              ))}
             </SimpleGrid>
           </Box>
         </Box>
